0571cfee4b9b643d39746242a8af901dd04bc2fb
JeanNguon <jeannguon@gmail.com>
2015-07-31 15:24:36 +0200

    jean notification v0.4

---
+++ src/Imie/SkillsBundle/Form/NotificationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Imie\SkillsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class NotificationType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('notificationName')
+                ->add('notificationDescription')
+                ->add('valider', 'submit')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Imie\SkillsBundle\Entity\Notification'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'imie_skillsbundle_notification';
+    }
+
+}
