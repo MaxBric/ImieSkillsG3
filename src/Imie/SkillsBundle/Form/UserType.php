2aacc25589a5ad37a2ea07992a154dad07a1c00b
JeanNguon <jeannguon@gmail.com>
2015-09-18 09:19:48 +0200

    jean mise à jour sécurité 18/09/2015

--- src/Imie/SkillsBundle/Form/UserType.php
+++ src/Imie/SkillsBundle/Form/UserType.php
@@ -5,38 +5,38 @@ namespace Imie\SkillsBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Imie\SkillsBundle\Form\RankType;
 
-class UserType extends AbstractType
-{
+class UserType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('userFirstName')
-            ->add('userLastName')
-            ->add('userBirthday', 'date', array(
-              'format' => "ddMMyyyy"
-            ))
-            ->add('userPhoneNumber', 'text')
-            ->add('userMail', 'email')
-            ->add('userAddress')
-            ->add('userEnable')
-            ->add('userLogin')
-            ->add('userPassword', 'password')
-            ->add('userDescription')
-            ->add('image',  new ImageType())
-            ->add('Add', 'submit')
+                ->add('userFirstName')
+                ->add('userLastName')
+                ->add('roles', new RankType)
+                ->add('userBirthday', 'date', array(
+                    'format' => "ddMMyyyy"
+                ))
+                ->add('userPhoneNumber', 'text')
+                ->add('userMail', 'email')
+                ->add('userAddress')
+                ->add('userEnable')
+                ->add('userLogin')
+                ->add('userPassword', 'password')
+                ->add('userDescription')
+                ->add('image', new ImageType())
+                ->add('Add', 'submit')
         ;
     }
 
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Imie\SkillsBundle\Entity\User'
         ));
@@ -45,8 +45,8 @@ class UserType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'imie_skillsbundle_user';
     }
+
 }
