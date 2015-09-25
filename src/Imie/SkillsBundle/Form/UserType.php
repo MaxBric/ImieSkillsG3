07dbcd478f750a702cf58f53b957167b67178df7
Jocelyn Bernard <Joast18@gmail.com>
2015-09-25 16:05:44 +0200

    Petites modifs de rien du tout.

--- src/Imie/SkillsBundle/Form/UserType.php
+++ src/Imie/SkillsBundle/Form/UserType.php
@@ -15,33 +15,33 @@ class UserType extends AbstractType {
   public function buildForm(FormBuilderInterface $builder, array $options) {
     $builder
     ->add('userFirstName', null, array(
-                    'label' => 'Prénom :'
+      'label' => 'Prénom :'
     ))
     ->add('userLastName', null, array(
-                    'label' => 'Nom :'
+      'label' => 'Nom :'
     ))
     ->add('userBirthday', 'date', array(
       'format' => "ddMMyyyy",
-                    'label' => 'Date de naissance :'
+      'label' => 'Date de naissance :'
     ))
-                ->add('userPhoneNumber', 'integer', array(
-                    'label' => 'Numéro de téléphone :'
+    ->add('userPhoneNumber', 'integer', array(
+      'label' => 'Numéro de téléphone :'
     ))
     ->add('userAddress', null, array(
-                    'label' => 'Adresse postale :'
+      'label' => 'Adresse postale :'
     ))
-                ->add('roles')
-                ->add('email', 'email', array('label' => 'Email :', 'translation_domain' => 'FOSUserBundle'))
-                ->add('username', null, array('label' => 'Pseudo :', 'translation_domain' => 'FOSUserBundle'))
+    ->add('roles')
+    ->add('email', 'email', array('label' => 'Email :', 'translation_domain' => 'FOSUserBundle'))
+    ->add('username', null, array('label' => 'Pseudo :', 'translation_domain' => 'FOSUserBundle'))
     ->add('plainPassword', 'repeated', array(
       'type' => 'password',
       'options' => array('translation_domain' => 'FOSUserBundle'),
-                    'first_options' => array('label' => 'Mot de passe'),
-                    'second_options' => array('label' => 'Confirmer mot de passe'),
+      'first_options' => array('label' => 'Mot de passe :'),
+      'second_options' => array('label' => 'Confirmer le mot de passe :'),
       'invalid_message' => 'fos_user.password.mismatch',
     ))
     ->add('userDescription', null, array(
-                    'label' => 'Description :'
+      'label' => 'Description :'
     ))
     ->add('Valider', 'submit', array(
       'attr' => array('class' => 'btn btn-primary'),
