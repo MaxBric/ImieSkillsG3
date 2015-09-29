b8a3761f2217f5c7e4daadc823897a9b6b308c5b
Maxence BRICAULT <maxence.bricault@gmail.com>
2015-09-29 15:50:46 +0200

    maxence

--- src/Imie/SkillsBundle/Controller/UserController.php
+++ src/Imie/SkillsBundle/Controller/UserController.php
@@ -106,7 +106,7 @@ class UserController extends Controller {
         if ($id != $this->get('security.token_storage')->getToken()->getUser()->getId()) {
             if (false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
                 throw new AccessDeniedException();
-            } 
+            }
         }
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository('ImieSkillsBundle:UserSkill');
